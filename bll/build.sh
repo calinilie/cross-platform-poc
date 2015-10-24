567af5ea209d97d77324e3c6b5b88c5696478946
calinilie <calinilielaur@gmail.com>
2015-10-24 23:14:51 +0200

    use of wildcard selector instead of specifying all jars manually

--- bll/build.sh
+++ bll/build.sh
@@ -23,7 +23,9 @@ find target/test-classes -type f -name "*.java" > generated-sources.txt
 
 echo
 echo "Compiling bll/src and dependencies ..."
-javac @sources.txt @generated-sources.txt -d compiled -cp "target/sources/gson-2.3.1-sources.jar:target/sources/junit-4.12-sources.jar:target/sources/hamcrest-core-1.3-sources.jar:target/sources/dagger-2.0-sources.jar:target/sources/javax.inject-1-sources.jar"
+javac @sources.txt @generated-sources.txt -d compiled -cp "./target/sources/*"
+# javac @sources.txt @generated-sources.txt -d compiled -cp "target/sources/gson-2.3.1-sources.jar:target/sources/junit-4.12-sources.jar:target/sources/hamcrest-core-1.3-sources.jar:target/sources/dagger-2.0-sources.jar:target/sources/javax.inject-1-sources.jar"
+# javac @sources.txt @generated-sources.txt -d compiled -cp "$POC_BLL_COMPILED/gson-2.3.1-sources.jar:$POC_BLL_COMPILED/junit-4.12-sources.jar:$POC_BLL_COMPILED/hamcrest-core-1.3-sources.jar:$POC_BLL_COMPILED/dagger-2.0-sources.jar:$POC_BLL_COMPILED/javax.inject-1-sources.jar"
 
 echo
 echo "Running tests ..."
